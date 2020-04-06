68a0b3bbec51cd658136538362ae1a90b12fb1d0
Aman Bhardwaj <amanbhardwaj@Amans-MacBook-Air.local>
2020-04-07 04:04:27 +0530

    2.0 v finished

--- seeder.js
+++ seeder.js
@@ -32,10 +32,10 @@ const reviews = JSON.parse(fs.readFileSync(`${__dirname}/_data/reviews.json`, 'u
 // import into db
 const importData = async () => {
   try {
-    await Bootcamp.create(bootcamps);
-    await Course.create(courses);
-    await User.create(users);
-    await Review.create(reviews);
+    // await Bootcamp.create(bootcamps);
+    // // await Course.create(courses);
+    // // await User.create(users);
+    // // await Review.create(reviews);
     console.log('Data imported'.green.inverse);
     process.exit();
   } catch (error) {
@@ -46,10 +46,10 @@ const importData = async () => {
 // delete into db
 const deleteData = async () => {
   try {
-    await Bootcamp.deleteMany();
-    await Course.deleteMany();
-    await User.deleteMany();
-    await Review.deleteMany();
+    // await Bootcamp.deleteMany();
+    // await Course.deleteMany();
+    // await User.deleteMany();
+    // await Review.deleteMany();
     console.log('Data destroyed'.red.inverse);
     process.exit();
   } catch (error) {
